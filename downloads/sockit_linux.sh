756ee191a4ed4e2dd4ae714c9868aacd85006591
Jon Tedesco <jtedesco@drw.com>
2011-08-03 09:48:03 -0500

    Updated firefox install script to use bzipped binaries

--- downloads/sockit_linux.sh
+++ downloads/sockit_linux.sh
@@ -4,10 +4,12 @@ version=`getconf LONG_BIT`
 
 if [ $version -eq "64" ] || [ $version -eq "32" ]
 then
-    url="http://sockit.github.com/downloads/npSockIt${version}.so"
+    url="http://sockit.github.com/downloads/npSockIt${version}.tar.bz"
     mkdir -p ~/.mozilla/plugins
     rm ~/.mozilla/plugins/npSockIt*.so
     wget $url
+    tar xzf npSockIt${version}.tar.bz
+    rm npSockIt${version}.tar.bz
     mv "npSockIt${version}.so" ~/.mozilla/plugins/
     echo "SockIt plugin successfully installed!"
 else
