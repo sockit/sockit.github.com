a161e9d8ac70df7003234c9083dddf65f70c69d4
Jon Tedesco <jtedesco@drw.com>
2011-08-03 08:37:53 -0500

    Cleaned up peer to peer chat demo code

--- demos/chat/chatUi.js
+++ demos/chat/chatUi.js
@@ -79,3 +79,32 @@ function keyup_chatter() {
 	}
 }
 
+
+$("#ip").focus();
+$("#username").hide();
+$("#usernamebutton").hide();
+$("#chatter").hide();
+$("#chatterbutton").hide();
+
+$("#ip").keyup(keyup_ip);
+$("#username").keyup(keyup_username);
+$("#chatter").keyup(keyup_chatter);
+
+$("#ip").click(function(event) {
+    if ($("#ip").val() == 'enter other ip') {
+        $("#ip").val("")
+    }
+});
+
+$("#username").click(function(event) {
+    if ($("#username").val() == 'username') {
+        $("#username").val("")
+    }
+});
+
+$("#chatter").click(function(event) {
+    if ($("#chatter").val() == 'type here') {
+        $("#chatter").val("")
+    }
+});
+
